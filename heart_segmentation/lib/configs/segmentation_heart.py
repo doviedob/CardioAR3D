6cb0af038e1f48d6347b4eee4161b43c65bd8789
doviedob <125707615+doviedob@users.noreply.github.com>
2024-08-29 08:24:28 -0500

    Add files via upload

---
+++ heart_segmentation/lib/configs/segmentation_heart.py
@@ -0,0 +1,100 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import os
+from typing import Any, Dict, Optional, Union
+
+import lib.infers
+import lib.trainers
+from monai.networks.nets import SegResNet
+from monai.utils import optional_import
+
+from monailabel.interfaces.config import TaskConfig
+from monailabel.interfaces.tasks.infer_v2 import InferTask
+from monailabel.interfaces.tasks.train import TrainTask
+from monailabel.utils.others.generic import download_file, strtobool
+
+_, has_cp = optional_import("cupy")
+_, has_cucim = optional_import("cucim")
+
+logger = logging.getLogger(__name__)
+
+
+class Segmentation_heart(TaskConfig):
+    def init(self, name: str, model_dir: str, conf: Dict[str, str], planner: Any, **kwargs):
+        super().init(name, model_dir, conf, planner, **kwargs)
+
+        # Labels
+        self.labels = {
+            "aorta": 1,
+            "miocardium": 2,
+            "left_atrium": 3,
+            "left_ventricle": 4,
+            "right_atrium": 5,
+            "right_ventricle": 6,
+            "pulmonar_artery": 7,
+        }
+
+        # Model Files
+        self.path = [
+            os.path.join(self.model_dir, f"pretrained_{name}.pt"),  # pretrained
+            os.path.join(self.model_dir, f"{name}.pt"),  # published
+        ]
+
+        # Download PreTrained Model
+        if strtobool(self.conf.get("use_pretrained_model", "false")):
+            url = f"{self.conf.get('pretrained_path', self.PRE_TRAINED_PATH)}"
+            url = f"{url}/radiology_segmentation_segresnet_multilabel.pt"
+            download_file(url, self.path[0])
+
+        self.target_spacing = (1.5, 1.5, 1.5)  # target space for image
+        # Setting ROI size - This is for the image padding
+        self.roi_size = (96, 96, 96)
+
+        # Network
+        self.network = SegResNet(
+            spatial_dims=3,
+            in_channels=1,
+            out_channels=len(self.labels) + 1,  # labels plus background,
+            init_filters=32,
+            blocks_down=(1, 2, 2, 4),
+            blocks_up=(1, 1, 1),
+            dropout_prob=0.2,
+        )
+
+    def infer(self) -> Union[InferTask, Dict[str, InferTask]]:
+        task: InferTask = lib.infers.Segmentation(
+            path=self.path,
+            network=self.network,
+            roi_size=self.roi_size,
+            target_spacing=self.target_spacing,
+            labels=self.labels,
+            preload=strtobool(self.conf.get("preload", "false")),
+            config={"largest_cc": True if has_cp and has_cucim else False},
+        )
+        return task
+
+    def trainer(self) -> Optional[TrainTask]:
+        output_dir = os.path.join(self.model_dir, self.name)
+        load_path = self.path[0] if os.path.exists(self.path[0]) else self.path[1]
+
+        task: TrainTask = lib.trainers.Segmentation(
+            model_dir=output_dir,
+            network=self.network,
+            roi_size=self.roi_size,
+            target_spacing=self.target_spacing,
+            load_path=load_path,
+            publish_path=self.path[1],
+            description="Train Segmentation Model",
+            labels=self.labels,
+        )
+        return task
